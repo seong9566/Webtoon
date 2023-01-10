b90c85622475d9aa02004a82337399c059385f21
seong9566 <dlgustjd9566@gmail.com>
2023-01-10 18:40:49 +0900

    웹툰 시자

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          backgroundColor: Colors.black,
+          body: Column(
+            children: [
+              Row(
+                children: [
+                  Column(
+                    children: [
+                      Text("Hey, Selena"),
+                      Text("Welcone back"),
+                    ],
+                  )
+                ],
+              ),
+            ],
+          )),
+    );
+  }
+}

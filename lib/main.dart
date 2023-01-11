4c05772fe173f91113a303d13c269eaed12824f1
seong9566 <dlgustjd9566@gmail.com>
2023-01-11 19:45:23 +0900

    위젯 트리, initState,dispose,build

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,13 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
+  bool showTitle = true;
+  void toggleTitle() {
+    setState(() {
+      showTitle = !showTitle;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -35,7 +42,11 @@ class _MyAppState extends State<MyApp> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              MyLargeTitle(),
+              showTitle ? MyLargeTitle() : Text("noting"),
+              IconButton(
+                onPressed: toggleTitle,
+                icon: Icon(Icons.remove_red_eye_rounded),
+              ),
             ],
           ),
         ),
@@ -54,8 +65,24 @@ class MyLargeTitle extends StatefulWidget {
 }
 
 class _MyLargeTitleState extends State<MyLargeTitle> {
+  //initState는 build가 되기 전가장 먼저 초기화 된다.
+  @override
+  void initState() {
+    super.initState();
+    print("initState");
+  }
+
+// 우리의 위젯, 위젯의 트리에서 없애거나
+////무언가를 취소, 화면에서 제거 되기 전 어떤 동작이 필요할 경우 사용.
+  @override
+  void dispose() {
+    super.dispose();
+    print("dispose");
+  }
+
   @override
   Widget build(BuildContext context) {
+    print("build");
     return Text(
       "My Large Title",
       style: TextStyle(

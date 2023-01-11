2b00fcb338720d19708cfd9bb633716b2d6e6c45
seong9566 <dlgustjd9566@gmail.com>
2023-01-11 16:56:45 +0900

    stateful 첫번째 예제 완료

--- lib/main.dart
+++ lib/main.dart
@@ -4,28 +4,50 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int counter = 0;
+  void onClicked() {
+    setState(() {
+      counter++;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.black,
-        body: Column(
-          children: [
-            Row(
-              children: [
-                Column(
-                  children: [
-                    Text("Hey, Selena"),
-                    Text("Welcone back"),
-                  ],
-                )
-              ],
-            ),
-          ],
+        backgroundColor: Color(0xFFF4EDDB),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Click Count",
+                style: TextStyle(fontSize: 30),
+              ),
+              Text(
+                "$counter",
+                style: TextStyle(fontSize: 30),
+              ),
+              IconButton(
+                onPressed: () {
+                  onClicked();
+                },
+                icon: Icon(
+                  Icons.add_box_outlined,
+                  size: 40,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
